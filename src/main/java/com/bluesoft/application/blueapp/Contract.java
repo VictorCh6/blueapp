5b63c1b691442109dd82ae662d9b63a88dbd7860
Victor Chukwu <victorchukwu@MacBook-Pro-Victor.local>
2017-11-03 18:30:11 +0100

    Added classes for System and Contract

---
+++ src/main/java/com/bluesoft/application/blueapp/Contract.java
@@ -0,0 +1,57 @@
+package com.bluesoft.application.blueapp;
+
+import java.util.Date;
+
+public class Contract {
+
+	String system;
+	Date startDate;
+	Date endDate;
+	Long value;
+	String  period;
+	Boolean isActive;
+	
+	String number;
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getSystem() {
+		return system;
+	}
+	public void setSystem(String system) {
+		this.system = system;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public Long getValue() {
+		return value;
+	}
+	public void setValue(Long value) {
+		this.value = value;
+	}
+	public String getPeriod() {
+		return period;
+	}
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+	public Boolean getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+}
---
+++ src/main/java/com/bluesoft/application/blueapp/SystemModel.java
@@ -0,0 +1,34 @@
+package com.bluesoft.application.blueapp;
+
+public class SystemModel {
+
+	String name;
+	String description;
+	String technologies;
+	String owner;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getTechnologies() {
+		return technologies;
+	}
+	public void setTechnologies(String technologies) {
+		this.technologies = technologies;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+}
