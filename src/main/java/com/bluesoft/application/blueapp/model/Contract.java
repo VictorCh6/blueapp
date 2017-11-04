2a21e32b888e2ebb4d4d2cb9cbb32242ecac714e
Victor Chukwu <victorchukwu@MacBook-Pro-Victor.local>
2017-11-05 00:54:41 +0100

    Added Entity information for hibernate

--- src/main/java/com/bluesoft/application/blueapp/model/Contract.java
+++ src/main/java/com/bluesoft/application/blueapp/model/Contract.java
@@ -1,16 +1,44 @@
 package com.bluesoft.application.blueapp.model;
 
-import java.util.Date;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "CONTRACTS")
 public class Contract {
 
-	String number;
-	SystemModel system;
-	Date startDate;
-	Date endDate;
-	Long value;
-	String  period;
-	Boolean isActive;
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private Long id;
+	
+	@Column(name = "NUMBER", unique = true)
+	@Size(max = 20, min = 3, message= "{contract.number.invalid}")
+	private String number;
+	
+	@Column(name = "SYSTEM")
+	private String system;
+	
+	@Column(name = "START_DATE")
+	private String startDate;
+	
+	@Column(name = "END_DATE")
+	private String endDate;
+	
+	@Column(name = "VALUE")
+	private Long value;
+	
+	@Column(name = "PERIOD")
+	private String  period;
+	
+	@Column(name = "ACTIVE")
+	private Boolean isActive;
 
 	public String getNumber() {
 		return number;
@@ -18,22 +46,22 @@ public class Contract {
 	public void setNumber(String number) {
 		this.number = number;
 	}
-	public SystemModel getSystem() {
+	public String getSystem() {
 		return system;
 	}
-	public void setSystem(SystemModel system) {
+	public void setSystem(String system) {
 		this.system = system;
 	}
-	public Date getStartDate() {
+	public String getStartDate() {
 		return startDate;
 	}
-	public void setStartDate(Date startDate) {
+	public void setStartDate(String startDate) {
 		this.startDate = startDate;
 	}
-	public Date getEndDate() {
+	public String getEndDate() {
 		return endDate;
 	}
-	public void setEndDate(Date endDate) {
+	public void setEndDate(String endDate) {
 		this.endDate = endDate;
 	}
 	public Long getValue() {
@@ -53,5 +81,11 @@ public class Contract {
 	}
 	public void setIsActive(Boolean isActive) {
 		this.isActive = isActive;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 }
